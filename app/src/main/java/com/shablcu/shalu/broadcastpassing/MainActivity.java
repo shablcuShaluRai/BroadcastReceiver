2dc8fbe4b496a513ed6d86cd9585d1e73351b674
shalurai <shaluraics22@gmail.com>
2016-07-07 19:05:30 +0900

    broadcast receiver without using mainfest

---
+++ app/src/main/java/com/shablcu/shalu/broadcastpassing/MainActivity.java
@@ -0,0 +1,51 @@
+package com.shablcu.shalu.broadcastpassing;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MyBroadcastReceiver myReceiver;
+    IntentFilter intentFilter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myReceiver=new MyBroadcastReceiver();
+        intentFilter =new IntentFilter("MY_SPECIFIC_ACTION");
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//---register the receiver---
+        registerReceiver(myReceiver, intentFilter);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+//---unregister the receiver---
+        unregisterReceiver(myReceiver);
+    }
+
+
+
+    public void onClick(View view){
+
+        Intent intent=new Intent("MY_SPECIFIC_ACTION");
+        intent.putExtra("name","broadcast receiver");
+        sendBroadcast(intent);
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/shablcu/shalu/broadcastpassing/MyBroadcastReceiver.java
@@ -0,0 +1,21 @@
+package com.shablcu.shalu.broadcastpassing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent i) {
+
+        Toast.makeText(context,
+                "Received broadcast in MyBroadcastReceiver, " +
+                        " value received: " + i.getStringExtra("name"),
+                Toast.LENGTH_LONG).show();
+
+
+
+    }
+}
